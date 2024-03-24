b6ac11629326942dba5c38e72f1b2ae9e75443d1
Tamicktom <rickpokemon38@gmail.com>
2024-03-24 15:58:23 -0300

    saving

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,51 @@
-import { GET, POST } from "./http";
+//* Libraries imports
+import z from "zod";
 
-const spell = {
-  GET,
-  POST,
-};
+//* Local imports
+import { fixUrlEnd, fixUrlStart, mergeUrls } from "./utils";
 
-export default spell;
+export class Spellbinder {
+  private baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  public get(url: string) {
+    return fetch(mergeUrls(this.baseUrl, url), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  public post(url: string, data: any) {
+    return fetch(mergeUrls(this.baseUrl, url), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+
+  public put(url: string, data: any) {
+    return fetch(mergeUrls(this.baseUrl, url), {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+
+  public delete(url: string) {
+    return fetch(mergeUrls(this.baseUrl, url), {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
