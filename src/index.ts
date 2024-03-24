76c63e1acafc082f05f0f34922b1ba3f50c1e4cb
Tamicktom <rickpokemon38@gmail.com>
2024-03-24 16:11:20 -0300

    saving

--- src/index.ts
+++ src/index.ts
@@ -4,48 +4,62 @@ import z from "zod";
 //* Local imports
 import { fixUrlEnd, fixUrlStart, mergeUrls } from "./utils";
 
+type PostArgs<T> = {
+  url: string;
+  body: string | object;
+  headers?: HeadersInit;
+  schema: z.ZodSchema<T>;
+};
+
+export const getDefaultHeaders = (): HeadersInit => {
+  const defaultHeaders = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+  return defaultHeaders;
+};
+
+class SpellError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "SpellError";
+  }
+}
+
+type ConstructorArgs = {
+  baseUrl: string;
+  defaultHeaders?: () => HeadersInit;
+};
+
 export class Spellbinder {
   private baseUrl: string;
+  private defaultHeaders = getDefaultHeaders;
 
-  constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
+  constructor(args: ConstructorArgs) {
+    this.baseUrl = args.baseUrl;
+    if (args.defaultHeaders) {
+      this.defaultHeaders = args.defaultHeaders;
+    }
   }
 
-  public get(url: string) {
-    return fetch(mergeUrls(this.baseUrl, url), {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+  async Post<T>(params: PostArgs<T>): Promise<T> {
+    const url = mergeUrls(this.baseUrl, params.url);
+    const headers = params.headers || this.defaultHeaders();
+    const body = params.body;
 
-  public post(url: string, data: any) {
-    return fetch(mergeUrls(this.baseUrl, url), {
+    const response = await fetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+      body: JSON.stringify(body),
+      headers,
     });
-  }
 
-  public put(url: string, data: any) {
-    return fetch(mergeUrls(this.baseUrl, url), {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-  }
+    const data = await response.json();
 
-  public delete(url: string) {
-    return fetch(mergeUrls(this.baseUrl, url), {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const validated = params.schema.safeParse(data);
+    if (!validated.success) {
+      throw new SpellError(validated.error.message);
+    }
+
+    return validated.data;
   }
 }
