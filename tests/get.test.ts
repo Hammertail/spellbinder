a3439b6be91cdd88088d8d6e087dfaad3f7dffb0
Tamicktom <rickpokemon38@gmail.com>
2024-03-28 10:58:31 -0300

    Update Spellbinder Get test with correct data and add error handling for incorrect schema

--- tests/get.test.ts
+++ tests/get.test.ts
@@ -7,7 +7,7 @@ import z from "zod";
 import { Spellbinder, SpellError } from "../src";
 
 test("Spellbinder Get test", async () => {
-  await it("should return the correct data", async () => {
+  await it("should return the correct data from github", async () => {
     const url = "/users/tamicktom";
     const spellbinder = new Spellbinder({ baseUrl: "https://api.github.com" });
 
@@ -27,5 +27,25 @@ test("Spellbinder Get test", async () => {
     equal(response.login, "Tamicktom");
     equal(response.id, 60244227);
     equal(response.created_at, "2020-01-24T00:19:27Z");
+  });
+
+  await it("should throw an error if the schema is not correct", async () => {
+    const url = "/users/tamicktom";
+    const spellbinder = new Spellbinder({ baseUrl: "https://api.github.com" });
+
+    const schema = z.object({
+      login: z.string(),
+      id: z.number(),
+      followers: z.number(),
+      following: z.number(),
+      created_at: z.string(),
+      updated_at: z.string(),
+    });
+
+    try {
+      await spellbinder.Get({ url, schema: schema.array() });
+    } catch (error) {
+      assert(error instanceof SpellError);
+    }
   });
 });
