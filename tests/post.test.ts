3eb89969ff882036f9156f832d1c6302451f64b2
Tamicktom <rickpokemon38@gmail.com>
2024-04-04 13:52:19 -0300

    Update package version and add test for Spellbinder Post method

--- tests/post.test.ts
+++ tests/post.test.ts
@@ -6,4 +6,21 @@ import z from "zod";
 
 import { Spellbinder, SpellError } from "../src";
 
-test("Spellbinder Post test", async () => {});
+test("Spellbinder Post test", async () => {
+  await it("should return the correct data from github", async () => {
+    const spell = new Spellbinder({ baseUrl: "https://api.github.com/" });
+    const url = "/users";
+    const body = {};
+
+    const schema = z.object({
+      login: z.string(),
+      id: z.number(),
+      followers: z.number(),
+      following: z.number(),
+      created_at: z.string(),
+      updated_at: z.string(),
+    });
+
+    const response = await spell.Post({ url, schema, body });
+  });
+});
