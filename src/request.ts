44631e2cb9bcda012afbfa606ac383d4af3720f0
Henrique Fonseca <60244227+Tamicktom@users.noreply.github.com>
2024-04-26 15:06:05 +0000

    add safe http methods

--- src/request.ts
+++ src/request.ts
@@ -1,10 +1,11 @@
 //* Libraries imports
-import type z from "zod";
+import z from "zod";
 
 //* Local imports
 import {
   mergeUrls,
   validateData,
+  safeValidateData,
   getDefaultHeaders,
   createUrl
 } from "./utils";
@@ -27,6 +28,17 @@ interface GetArgs<T> extends BaseArgs<T> { }
 interface PutArgs<T> extends PostArgs<T> { }
 interface PatchArgs<T> extends PostArgs<T> { }
 interface DeleteArgs<T> extends GetArgs<T> { }
+
+type baseSafeReturn<T> = Promise<
+  {
+    success: true;
+    data: T;
+  }
+  | {
+    success: false;
+    error: z.ZodError;
+  }
+>;
 
 type ConstructorArgs = {
   baseUrl: string;
@@ -98,6 +110,49 @@ export class Spellbinder {
     return validatedData;
   }
 
+  public async safeGet<T>({
+    url,
+    schema,
+    headers,
+    params,
+    ...rest
+  }: GetArgs<T>): Promise<baseSafeReturn<T>> {
+    const requestUrl = params ? createUrl(this.baseUrl, url, params) : mergeUrls(this.baseUrl, url);
+    const requestHeaders = headers || this.defaultHeaders();
+
+    const response = await fetch(requestUrl, {
+      method: "GET",
+      headers: requestHeaders,
+      ...rest,
+    });
+
+    const data = await response.json();
+    const validatedData = safeValidateData(data, schema);
+
+    if (this.DEBUG) {
+      debugLogger({
+        httpMethod: "GET",
+        url: requestUrl,
+        headers: requestHeaders,
+        body: null,
+        response,
+      });
+    }
+
+    if (validatedData instanceof z.ZodError) {
+      return {
+        success: false,
+        error: validatedData,
+      };
+    }
+
+    return {
+      success: true,
+      data: validatedData,
+    }
+  }
+
+
   public async post<T>({
     url,
     schema,
@@ -138,6 +193,58 @@ export class Spellbinder {
     }
 
     return validatedData;
+  }
+
+  public async safePost<T>({
+    url,
+    schema,
+    headers,
+    body,
+    params,
+    ...rest
+  }: PostArgs<T>): Promise<baseSafeReturn<T>> {
+    const requestUrl = params ? createUrl(this.baseUrl, url, params) : mergeUrls(this.baseUrl, url);
+    const requestHeaders = headers || this.defaultHeaders();
+    const requestBody = parseBody(body);
+
+    if (requestBody instanceof FormData) {
+      if (requestHeaders) {
+        // @ts-ignore
+        requestHeaders["Content-Type"] = "multipart/form-data";
+      }
+    }
+
+    const response = await fetch(requestUrl, {
+      method: "POST",
+      body: requestBody,
+      headers: requestHeaders,
+      ...rest,
+    });
+
+    const data = await response.json();
+    const validatedData = safeValidateData(data, schema);
+
+    if (this.DEBUG) {
+      debugLogger({
+        httpMethod: "POST",
+        url: requestUrl,
+        headers: requestHeaders,
+        body: requestBody,
+        response,
+      });
+    }
+
+    if (validatedData instanceof z.ZodError) {
+      return {
+        success: false,
+        error: validatedData,
+      };
+    }
+
+    return {
+      success: true,
+      data: validatedData,
+    }
   }
 
   public async put<T>({
@@ -182,6 +289,58 @@ export class Spellbinder {
     return validatedData;
   }
 
+  public async safePut<T>({
+    url,
+    schema,
+    headers,
+    body,
+    params,
+    ...rest
+  }: PutArgs<T>): Promise<baseSafeReturn<T>> {
+    const requestUrl = params ? createUrl(this.baseUrl, url, params) : mergeUrls(this.baseUrl, url);
+    const requestHeaders = headers || this.defaultHeaders();
+    const requestBody = parseBody(body);
+
+    if (requestBody instanceof FormData) {
+      if (requestHeaders) {
+        // @ts-ignore
+        requestHeaders["Content-Type"] = "multipart/form-data";
+      }
+    }
+
+    const response = await fetch(requestUrl, {
+      method: "PUT",
+      body: requestBody,
+      headers: requestHeaders,
+      ...rest,
+    });
+
+    const data = await response.json();
+    const validatedData = safeValidateData(data, schema);
+
+    if (this.DEBUG) {
+      debugLogger({
+        httpMethod: "PUT",
+        url: requestUrl,
+        headers: requestHeaders,
+        body: requestBody,
+        response,
+      });
+    }
+
+    if (validatedData instanceof z.ZodError) {
+      return {
+        success: false,
+        error: validatedData,
+      };
+    }
+
+    return {
+      success: true,
+      data: validatedData,
+    }
+  }
+
   public async patch<T>({
     url,
     schema,
@@ -224,6 +383,58 @@ export class Spellbinder {
     return validatedData;
   }
 
+  public async safePatch<T>({
+    url,
+    schema,
+    headers,
+    body,
+    params,
+    ...rest
+  }: PatchArgs<T>): Promise<baseSafeReturn<T>> {
+    const requestUrl = params ? createUrl(this.baseUrl, url, params) : mergeUrls(this.baseUrl, url);
+    const requestHeaders = headers || this.defaultHeaders();
+    const requestBody = parseBody(body);
+
+    if (requestBody instanceof FormData) {
+      if (requestHeaders) {
+        // @ts-ignore
+        requestHeaders["Content-Type"] = "multipart/form-data";
+      }
+    }
+
+    const response = await fetch(requestUrl, {
+      method: "PATCH",
+      body: requestBody,
+      headers: requestHeaders,
+      ...rest,
+    });
+
+    const data = await response.json();
+    const validatedData = safeValidateData(data, schema);
+
+    if (this.DEBUG) {
+      debugLogger({
+        httpMethod: "PATCH",
+        url: requestUrl,
+        headers: requestHeaders,
+        body: requestBody,
+        response,
+      });
+    }
+
+    if (validatedData instanceof z.ZodError) {
+      return {
+        success: false,
+        error: validatedData,
+      };
+    }
+
+    return {
+      success: true,
+      data: validatedData,
+    }
+  }
+
   public async delete<T>({
     url,
     schema,
@@ -254,5 +465,47 @@ export class Spellbinder {
     }
 
     return validatedData;
+  }
+
+  public async safeDelete<T>({
+    url,
+    schema,
+    headers,
+    params,
+    ...rest
+  }: DeleteArgs<T>): Promise<baseSafeReturn<T>> {
+    const requestUrl = params ? createUrl(this.baseUrl, url, params) : mergeUrls(this.baseUrl, url);
+    const requestHeaders = headers || this.defaultHeaders();
+
+    const response = await fetch(requestUrl, {
+      method: "DELETE",
+      headers: requestHeaders,
+      ...rest,
+    });
+
+    const data = await response.json();
+    const validatedData = safeValidateData(data, schema);
+
+    if (this.DEBUG) {
+      debugLogger({
+        httpMethod: "DELETE",
+        url: requestUrl,
+        headers: requestHeaders,
+        body: null,
+        response,
+      });
+    }
+
+    if (validatedData instanceof z.ZodError) {
+      return {
+        success: false,
+        error: validatedData,
+      };
+    }
+
+    return {
+      success: true,
+      data: validatedData,
+    }
   }
 }
